7e29924689779a16472514b4a69523fd2816e473
A Srinibash Achary <126653603+asrinibash@users.noreply.github.com>
2024-01-27 11:21:07 +0530

    Added Repository files
    Added Order,Review and Adress repository files

---
+++ Backend-Spring/src/main/java/com/cutm/marketconnect/Repository/AddressRepository.java
@@ -0,0 +1,8 @@
+package com.cutm.marketconnect.repository;
+
+import com.cutm.marketconnect.modal.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
+}
