adb4be3cccda2e744beac28f02faafd8eda16cd9
A Srinibash Achary <126653603+asrinibash@users.noreply.github.com>
2024-01-27 11:15:53 +0530

    Added Request files

---
+++ Backend-Spring/src/main/java/com/cutm/marketconnect/Request/DeleteProductRequest.java
@@ -0,0 +1,7 @@
+package com.cutm.marketconnect.request;
+
+public class DeleteProductRequest {
+	
+//	private Long 
+
+}
--- Backend-Spring/src/main/java/com/cutm/marketconnect/Request/LoginRequest.java
+++ Backend-Spring/src/main/java/com/cutm/marketconnect/Request/LoginRequest.java
@@ -1,27 +1,42 @@
-package com.cutm.marketconnect.Request;
+package com.cutm.marketconnect.request;
 
 public class LoginRequest {
-    private  String email;
-    private String password;
-
-    LoginRequest(){
-    }
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	
+	private String email;
+	private String password;
+	
+	
+	public LoginRequest() {
+		// TODO Auto-generated constructor stub
+	}
 
 
+	public LoginRequest(String email, String password) {
+		super();
+		this.email = email;
+		this.password = password;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 
 }
