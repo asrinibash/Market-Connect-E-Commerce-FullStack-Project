47723c8c8134c911ed88249212ab347537f993c9
A Srinibash Achary <126653603+asrinibash@users.noreply.github.com>
2024-01-27 11:18:50 +0530

    Added Repository files
    Added Database related files

---
+++ Backend-Spring/src/main/java/com/cutm/marketconnect/Repository/CartItemRepository.java
@@ -0,0 +1,15 @@
+package com.cutm.marketconnect.repository;
+
+import com.cutm.marketconnect.modal.Cart;
+import com.cutm.marketconnect.modal.CartItem;
+import com.cutm.marketconnect.modal.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long>{
+
+	@Query("SELECT ci From CartItem ci Where ci.cart=:cart And ci.product=:product And ci.size=:size And ci.userId=:userId")
+	public CartItem isCartItemExist(@Param("cart")Cart cart,@Param("product")Product product,@Param("size")String size, @Param("userId")Long userId);
+	
+}
