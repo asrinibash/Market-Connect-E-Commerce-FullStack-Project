5e357dae9287c3f59ad5a99b7d4c802633fa3eb8
A Srinibash Achary <126653603+asrinibash@users.noreply.github.com>
2024-01-27 10:51:57 +0530

    Added Order controller

---
+++ Backend-Spring/src/main/java/com/cutm/marketconnect/Controller/OrderController.java
@@ -0,0 +1,57 @@
+package com.cutm.marketconnect.controller;
+
+import com.cutm.marketconnect.exception.OrderException;
+import com.cutm.marketconnect.exception.UserException;
+import com.cutm.marketconnect.modal.Address;
+import com.cutm.marketconnect.modal.Order;
+import com.cutm.marketconnect.modal.User;
+import com.cutm.marketconnect.service.OrderService;
+import com.cutm.marketconnect.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+	
+	private OrderService orderService;
+	private UserService userService;
+	
+	public OrderController(OrderService orderService,UserService userService) {
+		this.orderService=orderService;
+		this.userService=userService;
+	}
+	
+	@PostMapping("/")
+	public ResponseEntity<Order> createOrderHandler(@RequestBody Address spippingAddress,
+			@RequestHeader("Authorization")String jwt) throws UserException{
+		
+		User user=userService.findUserProfileByJwt(jwt);
+		Order order =orderService.createOrder(user, spippingAddress);
+		
+		return new ResponseEntity<Order>(order,HttpStatus.OK);
+		
+	}
+	
+	@GetMapping("/user")
+	public ResponseEntity< List<Order>> usersOrderHistoryHandler(@RequestHeader("Authorization") 
+	String jwt) throws OrderException, UserException{
+		
+		User user=userService.findUserProfileByJwt(jwt);
+		List<Order> orders=orderService.usersOrderHistory(user.getId());
+		return new ResponseEntity<>(orders,HttpStatus.ACCEPTED);
+	}
+	
+	@GetMapping("/{orderId}")
+	public ResponseEntity< Order> findOrderHandler(@PathVariable Long orderId, @RequestHeader("Authorization") 
+	String jwt) throws OrderException, UserException{
+		
+		User user=userService.findUserProfileByJwt(jwt);
+		Order orders=orderService.findOrderById(orderId);
+		return new ResponseEntity<>(orders,HttpStatus.ACCEPTED);
+	}
+
+}
